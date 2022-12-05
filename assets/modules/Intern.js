c9320a240824c3b3e0966c85a47bf37eaf2c6f57
Jacob Meyers <jacobmeyers6@outlook.com>
2022-12-05 08:52:43 -0800

    added intern class

--- assets/modules/Intern.js
+++ assets/modules/Intern.js
@@ -1,2 +1,16 @@
 const Employee = require("./Employee");
 
+class Intern extends Employee {
+    constructor(name, id, email, school) {
+        super(name, id, email);
+        this.school = school;
+    }
+}
+
+Intern.prototype.getSchool() {
+    return this.school;
+}
+
+Intern.prototype.getRole() {
+    return "Intern";
+}
