95765b076a2f6a391664465cedf4c5abbad0d68f
Jacob Meyers <jacobmeyers6@outlook.com>
2022-12-05 08:54:46 -0800

    added manager class

--- assets/modules/Manager.js
+++ assets/modules/Manager.js
@@ -1,2 +1,16 @@
 const Employee = require("./Employee");
 
+class Manager extends Employee {
+    constructor(name, id, email, officeNumber) {
+        super(name, id, email);
+        this.officeNumber = officeNumber;
+    }
+}
+
+Manager.prototype.getOfficeNumber = function() {
+    return this.officeNumber;
+}
+
+Manager.prototype.getRole = function() {
+    return "Manager";
+}
